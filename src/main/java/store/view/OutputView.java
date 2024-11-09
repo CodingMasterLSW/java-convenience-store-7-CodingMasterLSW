e71c16eff411c24fabfe46c8841589f940fc0e45
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-09 15:23:30 +0900

    refactor: 요구사항에 맞게 구매 상품 정보 출력 형식 변경

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -11,6 +11,11 @@ public class OutputView {
     private static final String CONTAIN_PRODUCT_MESSAGE = "현재 보유하고 있는 상품입니다.";
     private static final String BLANK = "";
     private static final String PRODUCT_INFO = "- %s %,d원 %s %s";
+    private static final String RECEIPT_BANNER = "==============W 편의점================";
+    private static final String PURCHASE_SCHEMA = "상품명      수량  금액";
+    private static final String PURCHASE_INFO = "%s     %s      %,d";
+    private static final String PURCHASE_DELIMITER = "====================================";
+    private static final String TOTAL_INFO = "상품명      %,d  %,d";
 
     private OutputView(){
     }
@@ -35,15 +40,15 @@ public class OutputView {
     }
 
     public void printTotalInfo(PurchaseDto purchaseDto) {
-        System.out.println(purchaseDto.getTotalPrice());
-        System.out.println(purchaseDto.getTotalQuantity());
+        printMessage(PURCHASE_DELIMITER);
+        System.out.printf(TOTAL_INFO, purchaseDto.getTotalQuantity(), purchaseDto.getTotalPrice());
     }
 
     public void printPurchaseInfo(List<PurchaseItemDto> purchaseItemDtos) {
+        printMessage(RECEIPT_BANNER);
+        printMessage(PURCHASE_SCHEMA);
         for (PurchaseItemDto purchaseItemDto : purchaseItemDtos) {
-            System.out.println(purchaseItemDto.getProductName());
-            System.out.println(purchaseItemDto.getPrice());
-            System.out.println(purchaseItemDto.getQuantity());
+            System.out.printf(PURCHASE_INFO, purchaseItemDto.getProductName(), purchaseItemDto.getQuantity(), purchaseItemDto.getPrice());
             System.out.println();
         }
 
